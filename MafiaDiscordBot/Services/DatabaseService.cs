bf395947e0326252ea625c7096ea7f126c61e3f2
David Slutsky <d6000d@gmail.com>
2020-10-05 23:25:03 +0300

    Add database caching (currently without clearing option)

--- MafiaDiscordBot/Services/DatabaseService.cs
+++ MafiaDiscordBot/Services/DatabaseService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
@@ -39,6 +40,7 @@ namespace MafiaDiscordBot.Services
             }
             
             _connection = new MySqlConnection(connectionStringBuilder.GetConnectionString(true));
+            _compiler = new MySqlCompiler();
             try
             {
                 _connection.Open();
@@ -67,64 +69,92 @@ namespace MafiaDiscordBot.Services
             Log.Debug("Database service successfully initialized");
         }
 
-        private T ParseResult<T>(DbDataReader reader)
+        private T ParseResult<T>(DbDataReader reader) where T : class, Model.IDatabaseObject
         {
-            bool HasColumn(DbDataReader dr, string columnName, out int ordinal)
+            try
             {
-                for (int i = 0; i < dr.FieldCount; i++)
+                if (!reader.HasRows) return null;
+                reader.Read();
+
+                bool HasColumn(DbDataReader dr, string columnName, out int ordinal)
                 {
-                    if (dr.GetName(i).Equals(columnName, StringComparison.InvariantCultureIgnoreCase))
+                    for (int i = 0; i < dr.FieldCount; i++)
                     {
-                        ordinal = i;
-                        return true;
+                        if (dr.GetName(i).Equals(columnName, StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            ordinal = i;
+                            return true;
+                        }
+                    }
+
+                    ordinal = -1;
+                    return false;
+                }
+
+                Type destinationType = typeof(T);
+                var empty = (T) Activator.CreateInstance(destinationType);
+                var props = destinationType.GetProperties();
+                var columns = reader.GetColumnSchema();
+                foreach (var prop in props)
+                {
+                    if (!prop.CanWrite) continue;
+                    var attributes = prop.GetCustomAttributes(false);
+                    if (attributes.Any(x => x is SqlIgnoreAttribute)) continue;
+                    var sqlColumnAttr = (SqlColumnAttribute) attributes.FirstOrDefault(x => x is SqlColumnAttribute);
+                    string sqlName = sqlColumnAttr?.Name ?? prop.Name;
+                    if (HasColumn(reader, sqlName, out int ordinal))
+                    {
+                        var sqlConverterAttr =
+                            (SqlConverterAttribute) attributes.FirstOrDefault(x => x is SqlConverterAttribute);
+                        var dbvalue = reader.IsDBNull(ordinal) ? null : reader[ordinal];
+                        object gstate = null;
+                        if (sqlConverterAttr != null)
+                        {
+                            (object value, object state) = sqlConverterAttr.ReadWithState(dbvalue);
+                            gstate = state;
+                            prop.SetValue(empty, value);
+                        }
+                        else if (columns[ordinal].DataType == prop.PropertyType)
+                            prop.SetValue(empty, dbvalue);
+
+                        var _afterParseAttr =
+                            (AfterSqlParseAttribute) attributes.FirstOrDefault(x => x is AfterSqlParseAttribute);
+                        if (_afterParseAttr?.MethodName != null)
+                            destinationType.GetMethods()
+                                .FirstOrDefault(m =>
+                                    m.Name == _afterParseAttr.MethodName && m.GetParameters().Length == 1)
+                                ?.Invoke(empty, new[] {gstate});
                     }
                 }
-                ordinal = -1;
-                return false;
-            }
-            
-            Type destinationType = typeof(T);
-            var empty = (T)Activator.CreateInstance(destinationType);
-            var props = destinationType.GetProperties();
-            var columns = reader.GetColumnSchema();
-            foreach (var prop in props)
-            {
-                if (!prop.CanWrite) continue;
-                var attributes = prop.GetCustomAttributes(false);
-                if (attributes.Any(x => x is SqlIgnoreAttribute)) continue;
-                var sqlColumnAttr = (SqlColumnAttribute)attributes.FirstOrDefault(x => x is SqlColumnAttribute);
-                string sqlName = sqlColumnAttr?.Name ?? prop.Name;
-                if (HasColumn(reader, sqlName, out int ordinal))
-                {
-                    var sqlConverterAttr = (SqlConverterAttribute)attributes.FirstOrDefault(x => x is SqlConverterAttribute);
-                    var dbvalue = reader.IsDBNull(ordinal) ? null : reader[ordinal];
-                    object gstate = null;
-                    if (sqlConverterAttr != null)
-                    {
-                        (object value, object state) = sqlConverterAttr.ReadWithState(dbvalue);
-                        gstate = state;
-                        prop.SetValue(empty, value);
-                    }
-                    else if (columns[ordinal].DataType.Equals(prop.PropertyType))
-                        prop.SetValue(empty, dbvalue);
 
-                    var _afterParseAttr = (AfterSqlParseAttribute)attributes.FirstOrDefault(x => x is AfterSqlParseAttribute);
-                    if (_afterParseAttr?.MethodName != null)
-                        destinationType.GetMethods().FirstOrDefault(m => m.Name == _afterParseAttr.MethodName && m.GetParameters().Length == 1)?.Invoke(empty, new[] { gstate });
+                reader.Close();
+
+                empty.Filled = true;
+                empty.LastAccessed = DateTime.UtcNow;
+
+                var afterParseAttr = destinationType.GetCustomAttributes(typeof(AfterSqlParseAttribute), false);
+                if (afterParseAttr.Length > 0)
+                {
+                    var attr = (AfterSqlParseAttribute) afterParseAttr[0];
+                    if (attr.MethodName != null)
+                        destinationType.GetMethods()
+                            .FirstOrDefault(m =>
+                                m.Name == attr.MethodName &&
+                                m.GetParameters().Length == (attr.SqlColumnsAsArgs?.Length ?? 0))?.Invoke(empty,
+                                attr.SqlColumnsAsArgs?.Select(cln =>
+                                {
+                                    if (HasColumn(reader, cln, out int ordinal)) return reader[ordinal];
+                                    return null;
+                                })?.ToArray());
                 }
-            }
 
-            var afterParseAttr = destinationType.GetCustomAttributes(typeof(AfterSqlParseAttribute), false);
-            if (afterParseAttr.Length > 0)
-            {
-                var attr = (AfterSqlParseAttribute)afterParseAttr[0];
-                if (attr.MethodName != null) destinationType.GetMethods().FirstOrDefault(m => m.Name == attr.MethodName && m.GetParameters().Length == (attr.SqlColumnsAsArgs?.Length ?? 0))?.Invoke(empty, attr.SqlColumnsAsArgs?.Select(cln =>
-                {
-                    if (HasColumn(reader, cln, out int ordinal)) return reader[ordinal];
-                    return null;
-                })?.ToArray());
+                return empty;
             }
-            return empty;
+            catch
+            {
+                reader.Dispose();
+                throw;
+            }
         }
 
         private string GetSqlColumnName<T>(string propertyName) => (typeof(T).GetProperty(propertyName)?.GetCustomAttributes(typeof(SqlColumnAttribute), false).FirstOrDefault() as SqlColumnAttribute)?.Name ?? propertyName;
@@ -146,16 +176,24 @@ namespace MafiaDiscordBot.Services
 
             public class Guilds : ContextBase
             {
+                private readonly ConcurrentDictionary<ulong, Model.Guild> _guilds;
+                
                 public Guilds(DatabaseService service) : base(service, "guilds")
                 {
+                    _guilds = new ConcurrentDictionary<ulong, Model.Guild>();
                 }
                 
                 public async Task<Model.Guild> GetGuild(ulong guildId)
                 {
                     if (!CanWork) return null;
-                    return _service.ParseResult<Model.Guild>(await _service.GetExecuter(new Query(_tableName)
-                            .Where(_service.GetSqlColumnName<Model.Guild>("ID"), guildId))
+                    if (_guilds.TryGetValue(guildId, out Model.Guild cachedGuild))
+                        return cachedGuild;
+                    
+                    cachedGuild = _service.ParseResult<Model.Guild>(await _service.GetExecuter(new Query(_tableName)
+                            .Where(_service.GetSqlColumnName<Model.Guild>("id"), guildId))
                         .ExecuteReaderAsync().ConfigureAwait(false));
+                    
+                    return cachedGuild == null ? null : _guilds.AddOrUpdate(guildId, cachedGuild, (KeyStatus, guild) => cachedGuild);
                 }
 
                 public Task<Model.Guild> GetGuild(Discord.IGuild guild) => GetGuild(guild.Id);
