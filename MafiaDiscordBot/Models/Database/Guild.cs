d2d4c149595727c2d8de7464f3e943e766bb031f
David Slutsky <d6000d@gmail.com>
2020-10-06 11:24:27 +0300

    Add a last modified property to db object interface

--- MafiaDiscordBot/Models/Database/Guild.cs
+++ MafiaDiscordBot/Models/Database/Guild.cs
@@ -5,19 +5,77 @@ namespace MafiaDiscordBot.Models.Database
 {
     public class Guild : IDatabaseObject
     {
+        private ulong _id;
+        private ulong _gameCategoryId;
+        private string _prefix;
+        private string _localization;
+
         [SqlColumn("id")]
-        public ulong ID { get; set; }
+        public ulong ID
+        {
+            get
+            {
+                if (Filled) LastAccessed = DateTime.Now;
+                return _id;
+            }
+            set
+            {
+                _id = value;
+                if (Filled)
+                    LastModified = LastAccessed = DateTime.Now;
+            }
+        }
 
         [SqlColumn("game_category")]
-        public ulong GameCategoryId { get; set; }
+        public ulong GameCategoryId
+        {
+            get
+            {
+                if (Filled) LastAccessed = DateTime.Now;
+                return _gameCategoryId;
+            }
+            set
+            {
+                _gameCategoryId = value;
+                if (Filled)
+                    LastModified = LastAccessed = DateTime.Now;
+            }
+        }
 
         [SqlColumn("prefix")]
-        public string Prefix { get; set; }
-        
+        public string Prefix
+        {
+            get
+            {
+                if (Filled) LastAccessed = DateTime.Now;
+                return _prefix;
+            }
+            set
+            {
+                _prefix = value;
+                if (Filled)
+                    LastModified = LastAccessed = DateTime.Now;
+            }
+        }
+
         [SqlColumn("localization")]
-        public string Localization { get; set; }
+        public string Localization
+        {
+            get
+            {
+                if (Filled) LastAccessed = DateTime.Now;
+                return _localization;
+            }
+            set
+            {
+                _localization = value;
+                if (Filled)
+                    LastModified = LastAccessed = DateTime.Now;
+            }
+        }
 
         public DateTime LastAccessed { get; set; }
+        public DateTime? LastModified { get; set; }
         public bool Filled { get; set; }
     }
 }
--- MafiaDiscordBot/Models/Database/IDatabaseObject.cs
+++ MafiaDiscordBot/Models/Database/IDatabaseObject.cs
@@ -5,6 +5,7 @@ namespace MafiaDiscordBot.Models.Database
     public interface IDatabaseObject
     {
         DateTime LastAccessed { get; set; }
+        DateTime? LastModified { get; set; }
         bool Filled { get; set; }
     }
 }
